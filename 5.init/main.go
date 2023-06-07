cede2554dbbe216b8276a32aae0e3f52aa158025
MrMao14 <1477672272@qq.com>
2023-06-07 15:40:17 +0800

    import导包及init初始化

--- 5.init/main.go
+++ 5.init/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"Go-Study/5.init/lib1"
-	"Go-Study/5.init/lib2"
+	// l1和l2起别名
+	l1 "Go-Study/5.init/lib1"
+	l2 "Go-Study/5.init/lib2"
+	// 只执行init方法 不使用包内部的方法
+	_ "fmt"
+	// 导入全部方法 并且可以直接调用方法执行(不建议)
+	//. "fmt"
 )
 
 func main() {
-	lib1.Lib1Test()
-	lib2.Lib2Test()
+	l1.Lib1Test()
+	l2.Lib2Test()
 	/*
 		lib1 init()
 		lib2 init()
